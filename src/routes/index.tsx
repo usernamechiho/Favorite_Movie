3120b41b2b781c5c43552d2e92b1fe25ace187e9
Chiho <minepanorama@naver.com>
2022-06-13 13:21:31 +0900

    INIT: 프로젝트 세팅

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<div>123</div>} />
+    </Routes>
+  )
+}
+
+export default App
